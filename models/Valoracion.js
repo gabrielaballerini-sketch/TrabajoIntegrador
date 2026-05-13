fa86e9e6cb1f2b838a68d6020d74a4a3cfeb96a0
gabrielaballerini-sketch <gabrielaballerini@gmail.com>
2026-05-13 13:23:18 -0300

    cambio de entidad x error, Valoracion

--- models/Valoracion.js
+++ models/Valoracion.js
@@ -1,9 +1,9 @@
 import { Model, DataTypes } from "sequelize";
 import sequelize from "./config.js";
 
-export class Like extends Model {}
+export class Valoracion extends Model {}
 
-Like.init(
+Valoracion.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -11,12 +11,30 @@ Like.init(
       primaryKey: true,
     },
 
-  },
+  
+
+
+puntaje:{
+type:DataTypes.INTEGER,
+allowNull:false,
+validate:{
+min:1,
+max:5,
+
+},
+},
+
+
+
+
+},
+
+
 
 {
     sequelize, 
-    modelName: 'Like', 
-    tableName: 'likes', 
+    modelName: 'Valoracion', 
+    tableName: 'Valoraciones', 
     createdAt: true, 
     updatedAt: false,
     
