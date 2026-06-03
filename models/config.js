73de4cbb03cf5ff48c63082d68806a7a7b6d009e
gabrielaballerini-sketch <gabrielaballerini@gmail.com>
2026-06-02 23:27:50 -0300

    conexión SSL en sequelize

--- models/config.js
+++ models/config.js
@@ -3,28 +3,20 @@ import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 import pg from 'pg';
 
-// Detecta si estou en Vercel usando la variable DB_SSL que agregue
-const sslConn = process.env.DB_SSL === 'true' ? {
-    ssl: {
-
-        require: true,
-
-        rejectUnauthorized: false,
-    }
-} : undefined;
-
-// Configuración de Sequelize adaptada para la nube
-
-
 const sequelize = new Sequelize({
     dialect: 'postgres',
-    dialectModule: pg, 
-    dialectOptions: sslConn,
+    dialectModule: pg,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        }
+    }
 });
 
 
