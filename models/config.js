3af156a98060ac05a8ededa358ab4ea885b1cb78
gabrielaballerini-sketch <gabrielaballerini@gmail.com>
2026-06-03 01:36:58 -0300

    estabilización de base de datos

--- models/config.js
+++ models/config.js
@@ -1,24 +1,26 @@
 import 'dotenv/config';
-
 import { Sequelize } from 'sequelize';
 import pg from 'pg';
 
-const sequelize = new Sequelize({
-    dialect: 'postgres',
-    dialectModule: pg,
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    dialectOptions: {
+const sequelize = process.env.POSTGRES_URL
+  ? new Sequelize(process.env.POSTGRES_URL, {
+      dialect: 'postgres',
+      dialectModule: pg,
+      dialectOptions: {
         ssl: {
-            require: true,
-            rejectUnauthorized: false,
+          require: true,
+          rejectUnauthorized: false,
         }
-    }
-});
-
-
+      }
+    })
+  : new Sequelize({
+      dialect: 'postgres',
+      dialectModule: pg,
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
 
 export default sequelize;
